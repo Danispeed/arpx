d1ac213f10318e5336f265b935020a18fac0d42f
andreas <andreasnergaard02@gmail.com>
2026-04-18 19:45:44 +0200

    use environment variable for api version

--- agents/analyzer.py
+++ agents/analyzer.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_KEY"),
-    api_version="2024-02-01",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
 
